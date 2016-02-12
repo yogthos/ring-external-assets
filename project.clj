007bc423cba0afa80d52b3ca00c58a6ce584d5f7
Dmitri Sotnikov <dmitri.sotnikov@gmail.com>
2016-02-12 11:42:56 -0500

    Update project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject ring-external-assets "0.1.0"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject ring-external-assets "0.1.1"
+  :description "a library from loading public assets from a location on the filesystem"
+  :url "https://github.com/yogthos/ring-external-assets"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
